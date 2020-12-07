814b8764eaf57e4dac11946521b956c1b3effe51
shiroyuki28 <tampos.emmanuel@gmail.com>
2020-12-07 22:04:49 +0800

    added new routes

--- routes/chats.routes.js
+++ routes/chats.routes.js
@@ -12,12 +12,29 @@ router.get('/room/:room_id',validate([
         .notEmpty().withMessage('Invalid Room ID')]),
         chats_controller.getMessageByRoomID);
 
+router.get('/room/:room_id/:limit',validate([
+    param(['room_id','limit'])
+        .notEmpty().withMessage('Invalid Room ID')]),
+        chats_controller.getMessageByRoomIDLimit);
+
 router.get('/rooms/:uid1',validate([
     param('uid1')
-        .notEmpty().withMessage('Invalid Room ID')]),
+        .notEmpty().withMessage('Invalid User ID')]),
         chats_controller.getRoomsByUserID);
 
 router.post('/',chats_controller.newMessage);
+router.put('/msg/:msg_id',validate([
+    param('msg_id')
+        .notEmpty().withMessage('Invalid Message ID')]),
+        chats_controller.updateMessageByMsgID);
+router.delete('/msg/:msg_id',validate([
+    param('msg_id')
+        .notEmpty().withMessage('Invalid Message ID')]),
+        chats_controller.deleteMessageByMsgID)
 
-
+router.delete('/msg/:room_id',validate([
+    param('room_id')
+        .notEmpty().withMessage('Invalid Room ID')]),
+        chats_controller.deleteMessageByRoomID)
+        
 export default router
