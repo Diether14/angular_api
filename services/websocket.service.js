58bd59408cbefdd0ebb8ac6c269bad925a27b6cb
shiroyuki28 <tampos.emmanuel@gmail.com>
2020-12-07 22:05:26 +0800

    added ping to client function

--- services/websocket.service.js
+++ services/websocket.service.js
@@ -8,16 +8,21 @@ webServer.clientID = ()=>{
     }
     return s4() + s4() + '-' + s4();
 }
-
-
+function heartbeat(){
+    this.isAlive = true;
+}
+function noop(){}
 webServer.on('connection', socket=>{
     socket.id = webServer.clientID();
-
+    socket.isAlive =true;
+    socket.on('pong',heartbeat);
     console.log(socket.id)
     socket.on('message', message=> {
+        socket.on('pong',heartbeat);
         chats_controller.newMessage(JSON.parse(message))
     });
     socket.on('message',data=>{
+        socket.on('pong',heartbeat);
         console.log(JSON.parse(data))
         webServer.clients.forEach(function each(client){
             if(client.readyState ===  wss.OPEN){
@@ -26,4 +31,48 @@ webServer.on('connection', socket=>{
         })
     })
 });
+
+const interval = setInterval(function ping(){
+    webServer.clients.forEach( function each(socket){
+        if (socket.isAlive === false) return socket.terminate();
+        socket.isAlive = false;
+        socket.ping(noop);
+        // console.log(socket.isAlive)
+    });
+},30000)
+
+webServer.on('close',function close(){
+    clearInterval(interval);
+})
+
 export default webServer
+
+// import wss from 'ws';
+// import chats_controller from '../controllers/chats.controller.js'
+// const webServer = new wss.Server({noServer:true, path: "/chatws"});
+
+// webServer.clientID = ()=>{
+//     const s4 =()=> {
+//         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+//     }
+//     return s4() + s4() + '-' + s4();
+// }
+
+
+// webServer.on('connection', socket=>{
+//     socket.id = webServer.clientID();
+
+//     console.log(socket.id)
+//     socket.on('message', message=> {
+//         chats_controller.newMessage(JSON.parse(message))
+//     });
+//     socket.on('message',data=>{
+//         console.log(JSON.parse(data))
+//         webServer.clients.forEach(function each(client){
+//             if(client.readyState ===  wss.OPEN){
+//                 client.send(data)
+//             }
+//         })
+//     })
+// });
+// export default webServer
