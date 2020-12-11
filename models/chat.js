dfde5d6669cbae0475639d58422e9f86c24a0d42
shiroyuki28 <tampos.emmanuel@gmail.com>
2020-12-12 02:14:12 +0800

    merged chatgroup model with chats

--- models/chat.js
+++ models/chat.js
@@ -1,7 +1,8 @@
 import Model from './model.js';
 export default class Chat extends Model {
-    table = 'chat_room';
-    table2 = 'chat_messages'
+    table = 'chat_groups';
+    table2 = 'chat_participants'
+    table3 = 'chat_messages'
     constructor() {
         super();
     }
