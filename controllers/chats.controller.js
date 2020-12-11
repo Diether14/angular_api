45308a8259c6c8a0cf6b64ed6cf762026c26c9b5
shiroyuki28 <tampos.emmanuel@gmail.com>
2020-12-12 02:13:56 +0800

    merged chatgroup controller with chats

--- controllers/chats.controller.js
+++ controllers/chats.controller.js
@@ -4,11 +4,35 @@ const {matchedData} =validator;
 const repo = new ChatRepository();
 
 export default {
-
+    //websocket
     newMessage(req) {
         repo.newMessage(req)
     },
+    createNewGroup(req) {
+        repo.createNewGroup(req);
+    },
+    updateTime(req) {
+        repo.updateTime(req);
+    },
+    
 
+    
+    // getRoomsByUserID(req, res) {
+    //     var a
+    //     // const validated = matchedData(req, { locations: ['params'] });
+        
+    //     // console.log({validated})
+    //     repo.getRoomsByUserID(req).then(response => {
+    //         console.log (response)
+    //         // a= response;
+    //         // req.session.user = response.data;
+    //         // res.status(200).json(response);
+    //     }).catch(err => {
+    //         res.status(500).json(err);
+    //     });
+    //     // console.log(a)
+    // },
+    //http
     getRoomsByUserID(req, res) {
         const validated = matchedData(req, { locations: ['params'] });
         console.log({validated})
