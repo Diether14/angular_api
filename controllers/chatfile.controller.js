f4973c51dcbd89dff30e6ea3bdf84cad7a89294e
shiroyuki28 <tampos.emmanuel@gmail.com>
2020-12-18 22:11:04 +0800

    updated chatfile controller added getFileById

--- controllers/chatfile.controller.js
+++ controllers/chatfile.controller.js
@@ -12,4 +12,20 @@ export default{
             })
         })        
     },
+    newFileHttp(req, res) {
+        repo.newFileHttp(req).then(response => {
+            // req.session.user = response.data;
+            res.status(200).json(response);
+        }).catch(err => {
+            res.status(500).json(err);
+        });
+    },
+    getFileByID(req, res) {
+        repo.getFileByID(req).then(response => {
+            // req.session.user = response.data;
+            res.status(200).json(response);
+        }).catch(err => {
+            res.status(500).json(err);
+        });
+    },
 }
