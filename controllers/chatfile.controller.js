e3973aafeed4f64592764aadf58da55c0e3afa7d
shiroyuki28 <tampos.emmanuel@gmail.com>
2020-12-21 22:15:36 +0800

    added newMessageFile on chatfile controller

--- controllers/chatfile.controller.js
+++ controllers/chatfile.controller.js
@@ -4,13 +4,16 @@ const {matchedData} =validator;
 const repo = new ChatFileRepository();
 
 export default{
-    newFile(req, res) {
-        // const validated = matchedData(req, { locations: ['params'] });
-        return new Promise(async (resolve) => {
-            await repo.newFile(req).then(response => {
-                resolve(response)
-            })
-        })        
+    // newFile(req, res) {
+    //     // const validated = matchedData(req, { locations: ['params'] });
+    //     return new Promise(async (resolve) => {
+    //         await repo.newFile(req).then(response => {
+    //             resolve(response)
+    //         })
+    //     })        
+    // },
+    newMessageFile(req) {
+        repo.newMessageFile(req)
     },
     newFileHttp(req, res) {
         repo.newFileHttp(req).then(response => {
@@ -21,7 +24,9 @@ export default{
         });
     },
     getFileByID(req, res) {
-        repo.getFileByID(req).then(response => {
+        const validated = matchedData(req, { locations: ['params'] });
+        console.log({validated})
+        repo.getFileByID(validated).then(response => {
             // req.session.user = response.data;
             res.status(200).json(response);
         }).catch(err => {
