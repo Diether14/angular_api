b8bd52364d66cf68d550013fbd12ffe2ff0eab02
shiroyuki28 <tampos.emmanuel@gmail.com>
2021-01-05 22:27:38 +0800

    created new service session.service.js

---
+++ services/session.service.js
@@ -0,0 +1,24 @@
+import db from '../services/database.service.js'
+import session from 'express-session'
+import MySQLStore from 'express-mysql-session';
+import mysql from 'mysql'
+const expire = 60*2000
+const optn = new db()
+const sessionStore = MySQLStore(session)
+
+const sessStore = new sessionStore({expiration:expire},mysql.createPool( optn.getCon()))
+var user_session= session({
+        secret: 'ZGVmYXVsdHNlY3JldA==',
+        store: sessStore,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+            sameSite: true,
+            maxAge: expire, // Time is in miliseconds
+            expires: new Date(Date.now()+expire)
+        },
+        saveUninitialized:true,
+        resave:false
+    })
+
+export default user_session;
