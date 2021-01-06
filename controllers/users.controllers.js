a99120160e8df5607065f1ae3e0bf05663958aa4
shiroyuki28 <tampos.emmanuel@gmail.com>
2021-01-06 22:19:55 +0800

    updated users.controller.js

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -5,7 +5,7 @@ const {matchedData} = validator;
 const user_repository = new UserRepository();
 
 export default {
-        
+       
     async getUsers(req,res){
         user_repository.getUsers().then(response => {
             res.status(response.code).json(response);
@@ -15,18 +15,24 @@ export default {
     },
     async login(req, res){
 
-        console.log(req)
-        // console.log(req.session)
+        // console.log(req)
+        console.log(req.session)
         const validated = matchedData(req, { locations: ['body'] });
         user_repository.login(validated).then(response => {
+
             req.session.regenerate(function(err) {
                 const sess= req.session
                 sess.cookie.id = response.id
                 sess.cookie.name= response.name 
                 sess.cookie.sessionID=req.sessionID
-                // console.log(req.session.cookie)
-                // res.cookie('id',req.sessionID)
-                // res.send('')
+                console.log(response.code)
+                // res.cookie('isLogin',1)
+                // res.cookie('nm',response.name)
+                // res.cookie('id',window.btoa(response.id))
+                res.status(response.code)
+                    .cookie('isLogin',1)
+                    .cookie('nm',response.name)
+                    .json(response)
             })
             
         }).catch(err => {
@@ -44,8 +50,11 @@ export default {
         });
     },
     async logout(req,res){
+        console.log(req.cookie)
         req.session.destroy(function(err) {
 
+            res.status(200)
         })
+        
     }
 }
