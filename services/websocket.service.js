b8352703814475b7e8f30860dedd72a5b2346e72
shiroyuki28 <tampos.emmanuel@gmail.com>
2021-01-12 19:50:06 +0800

    updated websocket.service.ts

--- services/websocket.service.js
+++ services/websocket.service.js
@@ -2,6 +2,7 @@
 import wss from 'ws';
 import chats_controller from '../controllers/chats.controller.js'
 import chatfile_controller from '../controllers/chatfile.controller.js'
+import crypto from '../services/crypto.service.js'
 import fs from 'fs'
 import { parse } from 'path';
 
@@ -38,7 +39,6 @@ webServer.on('connection', socket=>{
         
         const cookie=socket.reqHead.cookie
         var user =parseCookies(cookie)
-        // console.log(message);
         if(Buffer.isBuffer(message)){
             
             new Promise (async()=>{
@@ -66,8 +66,6 @@ webServer.on('connection', socket=>{
         if(m[0]){
             if(m[0].type==="newgroup"){
                 chats_controller.createNewGroup(JSON.parse(message))
-                
-                // console.log(m[0].type)
             }
         }
         else{
@@ -87,41 +85,31 @@ webServer.on('connection', socket=>{
                     const d = new Date();
                     chats_controller.updateTime({"curtime": d ,"room_id": m.room_id})
                 }
-                // console.log(m)
-                // chatfile_controller.newFile(JSON.parse(message))
-
             }
         }
     });
     socket.on('message',data=>{
-        // socket.on('pong',heartbeat);
-        
         const cookie=socket.reqHead.cookie
         var user =parseCookies(cookie)
         const m =JSON.parse(data)
         console.log(m)
         if(!m[0]){
             if(m.type ==="message"){
-                // console.log(data)
-                // new Promise (async()=>{
-                //     // data.push("test")
-                //     var test = await chats_controller.getRoomsByUserID(JSON.parse(data))
-                //     const m =[JSON.parse(data)]
-                //     m.push(test.data)
-                //     webServer.clients.forEach(function each(client){
-                //         if(client.readyState ===  wss.OPEN){
-                //             client.send(JSON.stringify(m))
-                //         }
-                //     })
-                //  } )
+                webServer.clients.forEach(function each(client){
+                    if(client.readyState ===  wss.OPEN){
+                        console.log("sending")
+                        // console.log(m)
+                        const newdata = m;
+                        newdata['sender_id']= user['id']
+                        client.send(JSON.stringify(newdata))
+                    }
+                })
             }
         }
         if(m.type ==="rooms"){
             new Promise (async()=>{
-                // console.log(data)
                 const newdata = JSON.parse(data)
                 newdata["currentUser"]=user["id"]
-                // console.log(newdata)
                 var rooms = await chats_controller.getRoomsByUserID(newdata)
                 const m =[JSON.parse(data)]
                 m.push(rooms.data)
@@ -134,13 +122,6 @@ webServer.on('connection', socket=>{
                 })
              } )
         }
-        //test
-        // webServer.clients.forEach(function each(client){
-        //     if(client.readyState ===  wss.OPEN){
-        //         // console.log(data)
-        //         client.send(JSON.stringify(data))
-        //     }
-        // })
     })
 });
 
@@ -149,7 +130,6 @@ const interval = setInterval(function ping(){
         if (socket.isAlive === false) return socket.terminate();
         socket.isAlive = false;
         socket.ping(noop);
-        // console.log(socket.isAlive)
     });
 },30000)
 
@@ -158,37 +138,6 @@ webServer.on('request',function (req){
     
 })
 webServer.on('close',function close(){
-    // clearInterval(interval);
 })
 
 export default webServer
-
-// import wss from 'ws';
-// import chats_controller from '../controllers/chats.controller.js'
-// const webServer = new wss.Server({noServer:true, path: "/chatws"});
-
-// webServer.clientID = ()=>{
-//     const s4 =()=> {
-//         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-//     }
-//     return s4() + s4() + '-' + s4();
-// }
-
-
-// webServer.on('connection', socket=>{
-//     socket.id = webServer.clientID();
-
-//     console.log(socket.id)
-//     socket.on('message', message=> {
-//         chats_controller.newMessage(JSON.parse(message))
-//     });
-//     socket.on('message',data=>{
-//         console.log(JSON.parse(data))
-//         webServer.clients.forEach(function each(client){
-//             if(client.readyState ===  wss.OPEN){
-//                 client.send(data)
-//             }
-//         })
-//     })
-// });
-// export default webServer
